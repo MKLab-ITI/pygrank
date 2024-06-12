611a4453b86bdf4fd8e325d0e6ef5d6deab41c7f
maniospas <maniospas@hotmail.com>
2024-06-12 11:36:17 +0300

    improved evaluation docs

--- examples/playground/multigroup.py
+++ examples/playground/multigroup.py
@@ -1,7 +1,9 @@
 import pygrank as pg
 
 _, graph, communities = next(pg.load_datasets_multiple_communities(["dblp"]))
-algorithm = pg.PageRank(alpha=0.9, assume_immutability=True)  # cache graph preprocessing
+algorithm = pg.PageRank(
+    alpha=0.9, assume_immutability=True
+)  # cache graph preprocessing
 
 comm_scores = {name: algorithm(graph, members) for name, members in communities.items()}
 
@@ -9,4 +11,3 @@ import tqdm  # install this to be able to set it as a progress bar argument belo
 
 measure = pg.LinkAssessment(graph, progress=tqdm.tqdm)
 print(measure(comm_scores))
-
